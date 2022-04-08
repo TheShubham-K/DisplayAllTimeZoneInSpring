5d5ceb92b7d0a6371f7520478e2ec90baa8fbf16
Subham Kumar <subhamk@juniper.net>
2022-04-08 23:15:26 +0530

    executed all the time zone

--- src/main/java/com/shubham/DisplayAllTimeZone/DisplayAllTimeZoneApplication.java
+++ src/main/java/com/shubham/DisplayAllTimeZone/DisplayAllTimeZoneApplication.java
@@ -3,29 +3,44 @@ package com.shubham.DisplayAllTimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
 import java.util.List;
 
 @SpringBootApplication
-public class DisplayAllTimeZoneApplication {
+public class DisplayAllTimeZoneApplication{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DisplayAllTimeZoneApplication.class, args);
 
 		TimezoneDisplay display = new TimezoneDisplay();
 
 		System.out.println("Time zones in UTC:");
 		List<String> utc = display.getTimeZoneList(TimezoneDisplay.OffsetBase.UTC);
-		utc.forEach(System.out::println);
+//		utc.forEach(System.out::println);
+		try{
+			PrintStream file = new PrintStream(new File("utc.txt"));
+			System.setOut(file);
+			utc.forEach(file::print);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 
 		System.out.println("Time zones in GMT:");
 		List<String> gmt = display.getTimeZoneList(TimezoneDisplay.OffsetBase.GMT);
-		gmt.forEach(System.out::println);
-
+//		gmt.forEach(System.out::println);
+		try {
+			PrintStream file = new PrintStream(new File("gmt.txt"));
+			System.setOut(file);
+			gmt.forEach(file::print);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		/*
-		*
-		* Display the time zone in java7
-		*
-		* */
+		 *
+		 * Display the time zone in java7
+		 *
+		 */
 		TimezoneDisplayJava7 displayJava7 = new TimezoneDisplayJava7();
 
 		System.out.println("Time zones in UTC:");
